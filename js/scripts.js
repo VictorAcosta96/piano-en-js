373fc34ca0263fb7e38090a16351369fee62f1cd
victor acosta <victorhugo9602@hotmail.com>
2022-01-25 22:14:19 -0500

    piano en javascript

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => button.addEventListener('click', playSound));
+
+
+function playSound(event){
+
+    const button = event.target;
+    const note = button.dataset.note;
+
+
+    const audio = document.getElementById(`audio${note}`);
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
+}
+
+document.addEventListener('keydown', event =>{
+    const key = event.key;
+    console.log(key);
+    const button =document.querySelector(`button[data-key="${key}"]`);
+
+    if(button){
+        button.click();  
+    }
+})
